248b754de3983afde62a2b2cfab60d283244ab36
Emile <emile.georget@etuidiant.univ-rennes1.fr>
2020-01-31 15:22:32 +0100

    add main interfaces for Car

---
+++ src/com/kronos/api/RivalCar.java
@@ -0,0 +1,9 @@
+package com.kronos.api;
+
+/**
+ * @author @author TeamKronoS
+ * @version 1.0
+ * Represents the {@link RivalCar rival car} (contestant cars).
+ */
+public interface RivalCar extends Car {
+}
