75374237ce9d080d36218af3894fb590ec097bf4
Emile <emile.georget@etuidiant.univ-rennes1.fr>
2020-02-02 14:58:25 +0100

    add main model classes for car beginning of implementation

---
+++ src/com/kronos/model/RivalCarModel.java
@@ -0,0 +1,10 @@
+package com.kronos.model;
+
+import java.util.ArrayList;
+
+public class RivalCarModel extends CarModel {
+
+    public RivalCarModel(long id, int number, String name, String model, String brand, ArrayList<TopModel> topList, RaceModel race, int completedLaps) {
+        super(id, number, name, model, brand, topList, race, completedLaps);
+    }
+}
