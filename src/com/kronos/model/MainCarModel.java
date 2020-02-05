02aefcb1ba42fad6adbb5d0db4411b11ad7b34ba
Emile <emile.georget@etuidiant.univ-rennes1.fr>
2020-02-05 11:45:28 +0100

    add main function to check top logic in car controller fix main fixing code mistakes

--- src/com/kronos/model/MainCarModel.java
+++ src/com/kronos/model/MainCarModel.java
@@ -1,5 +1,7 @@
 package com.kronos.model;
 
+import com.kronos.api.TimeRace;
+
 import java.util.ArrayList;
 
 public class MainCarModel extends CarModel {
@@ -8,8 +10,8 @@ public class MainCarModel extends CarModel {
     private int remainingLapsBeforeStop;
 
 
-    public MainCarModel(long id, int number, String name, String model, String brand, ArrayList<TopModel> topList, RaceModel race, int completedLaps) {
-        super(id, number, name, model, brand, topList, race, completedLaps);
+    public MainCarModel(long id, int number, String name, String model, String brand, ArrayList<TopModel> topList, LapRaceModel lapRace, TimeRaceModel timeRace, int completedLaps) {
+        super(id, number, name, model, brand, topList, lapRace, timeRace, completedLaps);
         stopList = new ArrayList<>();
     }
 
