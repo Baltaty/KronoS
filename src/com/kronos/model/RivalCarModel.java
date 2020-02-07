0cb80b05407ffeda74edda108fa09f2a3012f997
Emile <emile.georget@etuidiant.univ-rennes1.fr>
2020-02-07 10:00:18 +0100

    add main empty constructors in model classes improve main separate constructors for Car object add main begin of data check on Car

--- src/com/kronos/model/RivalCarModel.java
+++ src/com/kronos/model/RivalCarModel.java
@@ -4,8 +4,16 @@ import java.util.ArrayList;
 
 public class RivalCarModel extends CarModel {
 
-    public RivalCarModel(long id, int number, String name, String model, String brand, ArrayList<TopModel> topList, LapRaceModel lapRace, TimeRaceModel timeRace, int completedLaps) {
-        super(id, number, name, model, brand, topList, lapRace, timeRace, completedLaps);
+    public RivalCarModel() {
+
+    }
+
+    public RivalCarModel(long id, int number, String name, String model, String brand, ArrayList<TopModel> topList, LapRaceModel lapRace, int completedLaps) {
+        super(id, number, name, model, brand, topList, lapRace, completedLaps);
+    }
+
+    public RivalCarModel(long id, int number, String name, String model, String brand, ArrayList<TopModel> topList, TimeRaceModel timeRace, int completedLaps) {
+        super(id, number, name, model, brand, topList, timeRace, completedLaps);
     }
 }
 
