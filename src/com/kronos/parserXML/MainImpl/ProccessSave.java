952d891b0fc7b0d02b0dc85bd689026465bc7274
oolbys <jeanolivier321@gmail.com>
2020-05-14 08:14:39 +0200

    fix bug save

--- src/com/kronos/parserXML/MainImpl/ProccessSave.java
+++ src/com/kronos/parserXML/MainImpl/ProccessSave.java
@@ -1,23 +1,6 @@
 package com.kronos.parserXML.MainImpl;
 
 import com.kronos.App;
-import com.kronos.model.LapRaceModel;
-import com.kronos.model.TopModel;
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.filter.ElementFilter;
-import org.jdom2.input.SAXBuilder;
-import org.jdom2.transform.JDOMSource;
-
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stream.StreamResult;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.util.Iterator;
 
 public class ProccessSave extends  Thread {
 
